79e928703cc446669447c4d3315e4c92fd26f349
iembro <gabriel.abromovicz@gmail.com>
2026-05-21 21:37:36 -0300

    Update login.php

--- login.php
+++ login.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <style>
 /*diego/*
 /*Vitor/*
-/*sergio*/
+/*gabriel*/
 /* Remove o brancão de fundo e centraliza a caixa de login na tela verticalmente */
 body {
     background-color: #121212 !important;
