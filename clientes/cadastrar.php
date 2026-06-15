d0f7e7f0648e1cfe4e14f5e5bf2bbb96bf95ae57
vito0o0 <vitor_pereira152@estudante.sesisenai.org.br>
2026-06-15 19:14:43 -0300

    Update cadastrar.php

--- clientes/cadastrar.php
+++ clientes/cadastrar.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../includes/functions.php'; 
 require_once '../includes/auth.php'; 
 
-// Inclui a conexão com o banco de dados Vitor
+// Inclui a conexão com o banco de dados
 include '../config/conexao.php'; 
 
 $mensagem = ''; 
