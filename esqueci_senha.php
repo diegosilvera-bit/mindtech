49732bb64aeda3b15d45998617029022ed911228
diegosilvera-bit <diego_silvera@estudante.sc.senai.br>
2026-07-14 19:26:14 -0300

    senha

--- esqueci_senha.php
+++ esqueci_senha.php
@@ -47,8 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mail->Host       = 'smtp.gmail.com';
             $mail->SMTPAuth   = true;
             $mail->Username   = 'themindtechservices@gmail.com'; // O seu novo e-mail
-            $mail->Password   = 'pblt frlp gbrw sudf
-'; // A senha de app do Google
+            $mail->Password   = 'pbltfrlpgbrwsudf'; // A senha de app do Google
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port       = 587;
 
